e84976d369176a90e48adb4bb905e920066fd832
Kimberly <kimberly@Administrators-MacBook-Pro.local>
2022-03-02 00:00:51 -0500

    Updates

---
+++ cb1608f2-f397d0720d411e3ba7e3.js
@@ -0,0 +1,3 @@
+/*! For license information please see cb1608f2-f397d0720d411e3ba7e3.js.LICENSE.txt */
+"use strict";(self.webpackChunkdigital_equity_report=self.webpackChunkdigital_equity_report||[]).push([[523],{8014:function(c,e,i){i.d(e,{q9j:function(){return t}});var r={prefix:"fas",iconName:"circle-up",icon:[512,512,[61467,"arrow-alt-circle-up"],"f35b","M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256c141.4 0 256-114.6 256-256S397.4 0 256 0zM382.8 246.1C380.3 252.1 374.5 256 368 256h-64v96c0 17.67-14.33 32-32 32h-32c-17.67 0-32-14.33-32-32V256h-64C137.5 256 131.7 252.1 129.2 246.1C126.7 240.1 128.1 233.3 132.7 228.7l112-112c6.248-6.248 16.38-6.248 22.62 0l112 112C383.9 233.3 385.3 240.1 382.8 246.1z"]},t=r}}]);
+//# sourceMappingURL=cb1608f2-f397d0720d411e3ba7e3.js.map
