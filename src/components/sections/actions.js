50009d33b4e8fa7bd31e6b553b9fbbf323ec7bd2
Tarin A. Comer <tcomer@mapc.org>
2022-03-17 11:30:36 -0400

    modify accordion change handling

--- src/components/sections/actions.js
+++ src/components/sections/actions.js
@@ -1,7 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
 import ActionsAccordion from "../accordions/actionsAccordion"
-import ImmediateActions from "../accordions/actionsAccordion1";
-import IntermediateActions from "../accordions/actionsAccordion2";
 
 const Actions = () => {
 
@@ -9,8 +7,6 @@ const Actions = () => {
     <div className="section section-bottom">
       <h2>Actions<br/>To Take</h2>
       <p>Below are actions that can be taken to address digital access and equity challenges facing our three communities. These recommendations range from immediate, intermediate, and long-term investments that can be made. These actions can build on each other to better and more sustainably connect residents and individuals in these communities.</p>
-      {/* <ImmediateActions />
-      <IntermediateActions /> */}
       <ActionsAccordion />
     </div>
   )
