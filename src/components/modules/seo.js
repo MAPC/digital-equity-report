98ffff923fc18ede58ecc851b5077585c06585c0
Kimberly <kimberly@Administrators-MacBook-Pro.local>
2022-03-25 16:14:28 -0400

    Test react-helmet for social cards meta tags

--- src/components/modules/seo.js
+++ src/components/modules/seo.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { Title, Link, Meta } from "react-head";
 import hero from "../../images/background.jpg"
+import {Helmet} from "react-helmet";
 
 const Seo = () => (
   <>
-    <Title>Digital Equity Report</Title>
-    <Meta name="description" content="This site is an online report on digital access and equity for Everett, Chelsea, and Revere. Digital equity for all is achievable." />
-    <Meta name="url" content="https://mapc.github.io/digital-equity-report/" />
-    <Meta name="image" content={hero} />
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Digital Equity Report</title>
+      <meta name="author" content="Metropolitan Area Planning Council" />
+      <meta name="description" content="This site is an online report on digital access and equity for Everett, Chelsea, and Revere. Digital equity for all is achievable." />
+      <meta name="keywords" content="fiber, digital, equity, internet, broadband" />
+      <meta property="og:title" content="Digital Equity Report" />
+      <meta property="og:description" content="This site is an online report on digital access and equity for Everett, Chelsea, and Revere. Digital equity for all is achievable." />
+      <meta property="og:image" content={hero} />
+      <meta property="og:url" content="https://mapc.github.io/digital-equity-report/" />
+    </Helmet>
   </>
 );
 
